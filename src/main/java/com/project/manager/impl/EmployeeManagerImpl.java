ae5dd1b46a6817199d686aa63f7a371ce45473dc
SunXiao <SunXiao@SunXiao-PC>
2016-05-18 17:53:43 +0800

    update log4j2

--- src/main/java/com/project/manager/impl/EmployeeManagerImpl.java
+++ src/main/java/com/project/manager/impl/EmployeeManagerImpl.java
@@ -1,5 +1,7 @@
 package com.project.manager.impl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,8 @@ import com.project.mapper.EmployeeMapper;
 
 @Service("employeeManager")
 public class EmployeeManagerImpl implements EmployeeManager{
+	
+	private static Logger log = LogManager.getLogger(EmployeeManagerImpl.class);
 
 	@Autowired
 	private EmployeeMapper employeeMapper;
@@ -16,6 +20,9 @@ public class EmployeeManagerImpl implements EmployeeManager{
 	@Override
 	public void save(Employee employee) {
 		employeeMapper.save(employee);
+		
+		log.info("保存数据成功！");
+		log.error("保存数据");
 	}
 
 }
