3917fad4deaaffcd5c69d4e3f36c0b3148319de0
SunXiao <SunXiao@192.168.0.250>
2016-05-27 19:28:00 +0800

    add Email,update mapper configure

--- src/main/java/com/project/manager/impl/WechatMemberManagerImpl.java
+++ src/main/java/com/project/manager/impl/WechatMemberManagerImpl.java
@@ -1,10 +1,15 @@
 package com.project.manager.impl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.project.entity.WechatMember;
 import com.project.manager.WechatMemberManager;
 
 public class WechatMemberManagerImpl implements WechatMemberManager{
 
+	private static Logger log = LogManager.getLogger(WechatMemberManagerImpl.class);
+	
 	public WechatMember getWechatMemberByUserId(String userId, String accountId) {
 		return null;
 	}
