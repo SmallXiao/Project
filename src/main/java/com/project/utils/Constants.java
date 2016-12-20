f72fbdb5c766be91ef6c1e805c32e17a0c93ef26
SunXiao <SunXiao@SunXiao-PC>
2016-12-20 17:14:52 +0800

    remove seeyon

--- src/main/java/com/project/utils/Constants.java
+++ src/main/java/com/project/utils/Constants.java
@@ -4,9 +4,7 @@ package com.project.utils;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by 高航 on 2014/8/5.
- */
+
 public final class Constants {
 
     public static String SEARCH_PER_KEY = "找";
@@ -32,32 +30,5 @@ public final class Constants {
     }
 
     public static List<String> pointlist = new ArrayList<String>();
-    public static enum Menu_Key {
-        /** 协同待办 */
-        V1001_COL_TODO,
-        /** 待开会议 */
-        V1002_MEET_TODO,
-        /** 新闻 */
-        V2001_NEWS,
-        /** 公告 */
-        V2002_BULLETIN,
-        /** 扫码登录 */
-        V2003_QRCODE,
-        /** 找人 */
-        V3001_SEARCH_MEM,
-        /** 今日事项 */
-        V1003_DATE,
-        /**帮助*/
-        V3004_HELP
-    }
-    public static enum WORKINDEX_TYPE {
-
-        工作狂人,
-
-        工作达人,
-
-        工作能手,
-
-        工作新兵
-    }
+    
 }
