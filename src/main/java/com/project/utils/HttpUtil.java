f2b721d1c0b51553fba1ed7f27b5f104276be09f
SunXiao <SunXiao@SunXiao-PC>
2016-12-20 17:15:18 +0800

    add http utils

--- src/main/java/com/project/utils/HttpUtil.java
+++ src/main/java/com/project/utils/HttpUtil.java
@@ -2,17 +2,43 @@ package com.project.utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.sf.json.JSONObject;
 
 public class HttpUtil {
 
+	private static final Logger log = LogManager.getLogger(HttpUtil.class);
+	
 	public static final String POST = "post"; // post请求
 	public static final String GET = "get"; // get请求
 
@@ -55,7 +81,7 @@ public class HttpUtil {
 			HttpURLConnection connection = openHttpURLConnection(url);// 获取http连接
 			connection.connect();// 建立与服务器的连接，并未发送数据
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					connection.getInputStream()));// 发送数据到服务器并使用Reader读取返回的数据
+					connection.getInputStream(), "UTF-8"));// 发送数据到服务器并使用Reader读取返回的数据
 			String line;
 			while ((line = reader.readLine()) != null) {
 				result.append(line);
@@ -105,5 +131,466 @@ public class HttpUtil {
 		}
 		return result.toString();
 	}
+	
+	public static JSONObject httpsRequest(String requestUrl,
+			String requestMethod, String outputStr) {
+		JSONObject jsonObject = null;
+		StringBuffer buffer = new StringBuffer();
+		try {
+			TrustManager[] tm = { new TrustManager() };
+			SSLContext sslContext = SSLContext.getInstance("SSL", "SunJSSE");
+			sslContext.init(null, tm, new SecureRandom());
+
+			SSLSocketFactory ssf = sslContext.getSocketFactory();
+
+			URL url = new URL(requestUrl);
+			HttpsURLConnection httpUrlConn = (HttpsURLConnection) url
+					.openConnection();
+			httpUrlConn.setSSLSocketFactory(ssf);
+			httpUrlConn.setHostnameVerifier(new HostnameVerifier() {
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			});
+
+			httpUrlConn.setDoOutput(true);
+			httpUrlConn.setDoInput(true);
+			httpUrlConn.setUseCaches(false);
+
+			httpUrlConn.setRequestMethod(requestMethod);
+			httpUrlConn.setConnectTimeout(5000);
+			httpUrlConn.setReadTimeout(5000);
+			if ("GET".equalsIgnoreCase(requestMethod)) {
+				httpUrlConn.connect();
+			}
+
+			if (outputStr != null) {
+				OutputStream outputStream = httpUrlConn.getOutputStream();
+
+				outputStream.write(outputStr.getBytes("UTF-8"));
+				outputStream.close();
+			}
+
+			InputStream inputStream = httpUrlConn.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(
+					inputStream, "utf-8");
+			BufferedReader bufferedReader = new BufferedReader(
+					inputStreamReader);
+
+			String str = null;
+			while ((str = bufferedReader.readLine()) != null) {
+				buffer.append(str);
+			}
+			bufferedReader.close();
+			inputStreamReader.close();
+
+			inputStream.close();
+			inputStream = null;
+			httpUrlConn.disconnect();
+			httpUrlConn = null;
+			jsonObject = JSONObject.fromObject(buffer.toString());
+		} catch (ConnectException ce) {
+			log.error("微信服务器连接超时！", ce);
+		} catch (Exception e) {
+			log.error("HTTPS请求错误!", e);
+		}
+		return jsonObject;
+	}
+	
+	public static JSONObject httpRequest(String requestUrl,
+			String requestMethod, String outputStr) {
+		JSONObject jsonObject = null;
+		StringBuffer buffer = new StringBuffer();
+		try {
+			// TrustManager[] tm = { new TrustManager() };
+
+			URL url = new URL(requestUrl);
+			HttpURLConnection httpUrlConn = (HttpURLConnection) url
+					.openConnection();
+
+			// httpUrlConn.setDoOutput(true);
+			// httpUrlConn.setDoInput(true);
+			// httpUrlConn.setUseCaches(false);
+
+			httpUrlConn.setRequestMethod(requestMethod);
+			httpUrlConn.setConnectTimeout(5000);
+			httpUrlConn.setReadTimeout(5000);
+			if ("GET".equalsIgnoreCase(requestMethod)) {
+				httpUrlConn.connect();
+			}
+
+			if (outputStr != null) {
+				OutputStream outputStream = httpUrlConn.getOutputStream();
+
+				outputStream.write(outputStr.getBytes("UTF-8"));
+				outputStream.close();
+			}
+
+			InputStream inputStream = httpUrlConn.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(
+					inputStream, "utf-8");
+			BufferedReader bufferedReader = new BufferedReader(
+					inputStreamReader);
+
+			String str = null;
+			while ((str = bufferedReader.readLine()) != null) {
+				buffer.append(str);
+			}
+			bufferedReader.close();
+			inputStreamReader.close();
+
+			inputStream.close();
+			inputStream = null;
+			httpUrlConn.disconnect();
+			httpUrlConn = null;
+			jsonObject = JSONObject.fromObject(buffer.toString());
+		} catch (ConnectException ce) {
+			log.error("微信服务器连接超时！", ce);
+		} catch (Exception e) {
+			log.error("HTTP请求错误!", e);
+		}
+		return jsonObject;
+	}
+
+	public static JSONObject httpGet(String url) {
+		HttpGet httpGet = new HttpGet(url);
+		CloseableHttpResponse response = null;
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		RequestConfig requestConfig = RequestConfig.custom()
+				.setSocketTimeout(2000).setConnectTimeout(2000).build();
+		httpGet.setConfig(requestConfig);
+
+		try {
+			response = httpClient.execute(httpGet, new BasicHttpContext());
+
+			if (response.getStatusLine().getStatusCode() != 200) {
+
+				System.out.println("request url failed, http code="
+						+ response.getStatusLine().getStatusCode() + ", url="
+						+ url);
+				return null;
+			}
+			HttpEntity entity = response.getEntity();
+			if (entity != null) {
+				String resultStr = EntityUtils.toString(entity, "utf-8");
+
+				JSONObject result = JSONObject.fromObject(resultStr);
+				return result;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (response != null)
+				try {
+					response.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+
+		return null;
+	}
+
+	public static JSONObject httpPost(String url, String msgContent)
+			throws UnsupportedEncodingException {
+		HttpPost httpPost = new HttpPost(url);
+		CloseableHttpResponse response = null;
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		RequestConfig requestConfig = RequestConfig.custom()
+				.setSocketTimeout(2000).setConnectTimeout(2000).build();
+		httpPost.setConfig(requestConfig);
+		httpPost.addHeader("Content-Type", "application/json");
+
+		StringEntity requestEntity = new StringEntity(msgContent, "UTF-8");
+		httpPost.setEntity(requestEntity);
+
+		try {
+			response = httpClient.execute(httpPost, new BasicHttpContext());
+
+			if (response.getStatusLine().getStatusCode() != 200) {
+				log.error("request url failed, http code="
+						+ response.getStatusLine().getStatusCode() + ", url="
+						+ url);
+				return null;
+			}
+			HttpEntity entity = response.getEntity();
+			if (entity != null) {
+				String resultStr = EntityUtils.toString(entity, "UTF-8");
+				JSONObject result = JSONObject.fromObject(resultStr);
+				return result;
+			}
+		} catch (Exception e) {
+			log.error("request url=" + url + ", exception, msg="
+					+ e.getMessage());
+			e.printStackTrace();
+		} finally {
+			if (response != null)
+				try {
+					response.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		return null;
+	}
+
+	public static String httpsRequestRest(String requestUrl,
+			String requestMethod, String outputStr) {
+		StringBuffer buffer = new StringBuffer();
+		try {
+			TrustManager[] tm = { new TrustManager() };
+			SSLContext sslContext = SSLContext.getInstance("SSL", "SunJSSE");
+			sslContext.init(null, tm, new SecureRandom());
+
+			SSLSocketFactory ssf = sslContext.getSocketFactory();
+
+			URL url = new URL(requestUrl);
+			HttpsURLConnection httpUrlConn = (HttpsURLConnection) url
+					.openConnection();
+			httpUrlConn.setSSLSocketFactory(ssf);
+			httpUrlConn.setHostnameVerifier(new HostnameVerifier() {
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			});
+
+			httpUrlConn.setDoOutput(true);
+			httpUrlConn.setDoInput(true);
+			httpUrlConn.setUseCaches(false);
+
+			httpUrlConn.setRequestMethod(requestMethod);
+			httpUrlConn.setConnectTimeout(5000);
+			httpUrlConn.setReadTimeout(5000);
+			if ("GET".equalsIgnoreCase(requestMethod)) {
+				httpUrlConn.connect();
+			}
+
+			if (outputStr != null) {
+				OutputStream outputStream = httpUrlConn.getOutputStream();
+
+				outputStream.write(outputStr.getBytes("UTF-8"));
+				outputStream.close();
+			}
+
+			InputStream inputStream = httpUrlConn.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(
+					inputStream, "utf-8");
+			BufferedReader bufferedReader = new BufferedReader(
+					inputStreamReader);
+
+			String str = null;
+			while ((str = bufferedReader.readLine()) != null) {
+				buffer.append(str);
+			}
+			bufferedReader.close();
+			inputStreamReader.close();
+
+			inputStream.close();
+			inputStream = null;
+			httpUrlConn.disconnect();
+			httpUrlConn = null;
+			// jsonObject = JSONObject.fromObject(buffer.toString());
+		} catch (ConnectException ce) {
+			log.error("微信服务器连接超时！", ce);
+		} catch (Exception e) {
+			log.error("HTTPS请求错误!", e);
+		}
+		return buffer.toString();
+	}
+
+	public static String httpRequestRest(String requestUrl,
+			String requestMethod, String outputStr) {
+		StringBuffer buffer = new StringBuffer();
+		try {
+			// TrustManager[] tm = { new TrustManager() };
+
+			URL url = new URL(requestUrl);
+			HttpURLConnection httpUrlConn = (HttpURLConnection) url
+					.openConnection();
+
+			// httpUrlConn.setDoOutput(true);
+			// httpUrlConn.setDoInput(true);
+			// httpUrlConn.setUseCaches(false);
+
+			httpUrlConn.setRequestMethod(requestMethod);
+			// httpUrlConn.setConnectTimeout(5000);
+			// httpUrlConn.setReadTimeout(5000);
+			if ("GET".equalsIgnoreCase(requestMethod)) {
+				httpUrlConn.connect();
+			}
+
+			if (outputStr != null) {
+				OutputStream outputStream = httpUrlConn.getOutputStream();
+
+				outputStream.write(outputStr.getBytes("UTF-8"));
+				outputStream.close();
+			}
+
+			InputStream inputStream = httpUrlConn.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(
+					inputStream, "UTF-8");
+			BufferedReader bufferedReader = new BufferedReader(
+					inputStreamReader);
+
+			String str = null;
+			while ((str = bufferedReader.readLine()) != null) {
+				buffer.append(str);
+			}
+			bufferedReader.close();
+			inputStreamReader.close();
+
+			inputStream.close();
+			inputStream = null;
+			httpUrlConn.disconnect();
+			httpUrlConn = null;
+			// jsonObject = JSONObject.fromObject(buffer.toString());
+		} catch (ConnectException ce) {
+			log.error("微信服务器连接超时！", ce);
+		} catch (Exception e) {
+			log.error("HTTP请求错误!", e);
+		}
+		return buffer.toString();
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static JSONObject httpRequestPost(String requestUrl,
+			String requestMethod, String outputStr, Map requestParamsMap) {
+		JSONObject jsonObject = null;
+		StringBuffer buffer = new StringBuffer();
+		StringBuffer params = new StringBuffer();
+		// 组织请求参数
+		Iterator it = requestParamsMap.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry element = (Map.Entry) it.next();
+			params.append(element.getKey());
+			params.append("=");
+			params.append(element.getValue());
+			params.append("&");
+		}
+		if (params.length() > 0) {
+			params.deleteCharAt(params.length() - 1);
+		}
+		try {
+			// TrustManager[] tm = { new TrustManager() };
+
+			URL url = new URL(requestUrl);
+			HttpURLConnection httpUrlConn = (HttpURLConnection) url
+					.openConnection();
+			PrintWriter printWriter = null;
+			// httpUrlConn.setDoOutput(true);
+			// httpUrlConn.setDoInput(true);
+			// httpUrlConn.setUseCaches(false);
+
+			httpUrlConn.setRequestMethod(requestMethod);
+			httpUrlConn.setConnectTimeout(5000);
+			httpUrlConn.setReadTimeout(5000);
+			if ("GET".equalsIgnoreCase(requestMethod)) {
+				httpUrlConn.connect();
+			}
+			if ("POST".equalsIgnoreCase(requestMethod)) {
+				httpUrlConn.setDoOutput(true);
+				httpUrlConn.setDoInput(true);
+				httpUrlConn.connect();
+				printWriter = new PrintWriter(httpUrlConn.getOutputStream());
+				// 发送请求参数
+				printWriter.write(params.toString());
+				// flush输出流的缓冲
+				printWriter.flush();
+				// 根据ResponseCode判断连接是否成功
+				int responseCode = httpUrlConn.getResponseCode();
+				if (responseCode != 200) {
+					log.error(" Error===" + responseCode);
+				} else {
+					log.info("Post Success!");
+				}
+			}
+
+			if (outputStr != null) {
+				OutputStream outputStream = httpUrlConn.getOutputStream();
+
+				outputStream.write(outputStr.getBytes("UTF-8"));
+				outputStream.close();
+			}
+
+			InputStream inputStream = httpUrlConn.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(
+					inputStream, "utf-8");
+			BufferedReader bufferedReader = new BufferedReader(
+					inputStreamReader);
+
+			String str = null;
+			while ((str = bufferedReader.readLine()) != null) {
+				buffer.append(str);
+			}
+			bufferedReader.close();
+			inputStreamReader.close();
+
+			inputStream.close();
+			inputStream = null;
+			httpUrlConn.disconnect();
+			httpUrlConn = null;
+			jsonObject = JSONObject.fromObject(buffer.toString());
+		} catch (ConnectException ce) {
+			log.error("微信服务器连接超时！", ce);
+		} catch (Exception e) {
+			log.error("HTTP请求错误!", e);
+		}
+		return jsonObject;
+	}
+	
+	public static JSONObject httpRequesttemp(String requestUrl,
+			String requestMethod, String outputStr) {
+		JSONObject jsonObject = null;
+		StringBuffer buffer = new StringBuffer();
+		try {
+			// TrustManager[] tm = { new TrustManager() };
+
+			URL url = new URL(requestUrl);
+			HttpURLConnection httpUrlConn = (HttpURLConnection) url
+					.openConnection();
+
+			// httpUrlConn.setDoOutput(true);
+			// httpUrlConn.setDoInput(true);
+			// httpUrlConn.setUseCaches(false);
+
+			httpUrlConn.setRequestMethod(requestMethod);
+
+			if ("GET".equalsIgnoreCase(requestMethod)) {
+				httpUrlConn.connect();
+			}
+
+			if (outputStr != null) {
+				OutputStream outputStream = httpUrlConn.getOutputStream();
+
+				outputStream.write(outputStr.getBytes("UTF-8"));
+				outputStream.close();
+			}
+
+			InputStream inputStream = httpUrlConn.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(
+					inputStream, "utf-8");
+			BufferedReader bufferedReader = new BufferedReader(
+					inputStreamReader);
+
+			String str = null;
+			while ((str = bufferedReader.readLine()) != null) {
+				buffer.append(str);
+			}
+			bufferedReader.close();
+			inputStreamReader.close();
+
+			inputStream.close();
+			inputStream = null;
+			httpUrlConn.disconnect();
+			httpUrlConn = null;
+			// jsonObject = JSONObject.fromObject(buffer.toString());
+		} catch (ConnectException ce) {
+			log.error("微信服务器连接超时！", ce);
+		} catch (Exception e) {
+			log.error("HTTP请求错误!", e);
+		}
+		return jsonObject;
+	}
+	
 
 }
